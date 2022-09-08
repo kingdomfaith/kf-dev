e07b08660489a5dbfb7308f20d2eee92faca698f
asains <46875138+asains@users.noreply.github.com>
2022-09-08 13:35:01 +0100

    Updates

--- global.js
+++ global.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
     $(".menu-toggle").click(function(e){
-        e.preventDefault()
         if ($(".menu").hasClass("active")) {
             $(".menu").removeClass("active");
         }
         else {
             $(".menu").addClass("active");
+            $(".dropdown-link").removeClass("active")
         }
       });
+
+      $(".dropdown-link").click(function(e){
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+        }
+        else {
+            $(this).addClass("active");
+        }       
+      });
+
 });
